663d4e85f86df605f0c9125f9d55e77d310dd2c6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-05-03 18:23:56 +0900

    lambdaのbuildスクリプトをそれらしく作ってみた

--- build.js
+++ build.js
@@ -1,21 +1,45 @@
 const fs = require('fs');
+const path = require('path');
 const archiver = require('archiver');
 const glob = require("glob");
 
-// create a file to stream archive data to.
-const output = fs.createWriteStream(__dirname + '/example.zip');
-const archive = archiver('zip');
-output.on('close', () => {
-  console.log(archive.pointer() + ' total bytes');
-  console.log('archiver has been finalized and the output file descriptor has closed.');
-});
-output.on('end', () => {
+const archiveCompresser = function (platformName, projectName) {
+  // create a file to stream archive data to.
+  const output = fs.createWriteStream(__dirname + '/' + [platformName, projectName].join('_') + '.zip');
+  const archive = archiver('zip');
+  output.on('close', () => {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+  });
+  output.on('end', () => {
     console.log('Data has been drained');
-});
-archive.on('warning', (err) =>  {throw err;});
-archive.on('error',  (err) =>  {throw err;});
-archive.pipe(output);
-// 圧縮対象ファイル
-const file1 = __dirname + '/file1.txt';
-archive.append(fs.createReadStream(file1), { name: 'file1.txt' });
-archive.finalize();
+  });
+  archive.on('warning', (err) => {
+    throw err;
+  });
+  archive.on('error', (err) => {
+    throw err;
+  });
+  archive.pipe(output);
+  const filePathes = glob.sync('./lambda/' + platformName + '/' + projectName + '/**/*.js');
+  for (const filePath of filePathes) {
+    const fileName = path.basename(filePath);
+    archive.append(fs.createReadStream(filePath), {
+      name: fileName
+    });
+  }
+
+  //archive.glob('./lambda/' + platformName + '/' + projectName + '/**/*.js');
+  archive.glob('./libs/**/*.js');
+  archive.glob('node_modules/**/');
+  archive.finalize();
+}
+
+const platformNames = ['LineBot', 'FacebookBot'];
+for (const plName of platformNames) {
+  const projectPathes = glob.sync('./lambda/' + plName + '/*');
+  for (const projectPath of projectPathes) {
+    const projectName = path.basename(projectPath);
+    archiveCompresser(plName, projectName);
+  }
+}
---
+++ lambda/LineBot/hiwaihub/index.js
@@ -0,0 +1,59 @@
+var LineBot = require(__dirname + '/linebot.js');
+
+var callLambdaResponse = function(promise, context){
+  promise.then((response) => {
+    var lambdaResponse = {
+      statusCode: 200,
+      headers: { "X-Line-Status": "OK"},
+      body: JSON.stringify({"result": "completed"})
+    };
+    context.succeed(lambdaResponse);
+  }).catch(function(err){
+    console.log(err);
+    console.log(JSON.stringify(err.originalError.response.data));
+  });
+}
+
+exports.handler = function (event, context) {
+  console.log(JSON.stringify(event));
+  var linebot = new LineBot(process.env.ACCESSTOKEN);
+  var lineClient = linebot.lineClient;
+  event.events.forEach(function(lineMessage) {
+    if(lineMessage.type == "follow"){
+      var followPromise = linebot.follow(lineMessage.source.userId, lineMessage.timestamp);
+      var promise = followPromise.then(function(userData){
+        return linebot.generateConfirmMessage();
+      }).then(function(confirmObj){
+        return lineClient.replyMessage(lineMessage.replyToken, confirmObj);
+      });
+      callLambdaResponse(promise, context);
+    }else if(lineMessage.type == "unfollow"){
+      callLambdaResponse(linebot.unfollow(lineMessage.source.userId, lineMessage.timestamp).then(function(){
+        //return linebot.unlinkRichMenu(lineMessage.source.userId, process.env.RICHMENUID1);
+      }), context);
+    }else if(lineMessage.type == "postback"){
+      var receiveData = JSON.parse(lineMessage.postback.data);
+      if(receiveData.confirmed){
+        callLambdaResponse(linebot.updateConfirmState(lineMessage.source.userId, lineMessage.timestamp).then(function(){
+          //return linebot.linkRichMenu(lineMessage.source.userId, process.env.RICHMENUID1)
+        }), context);
+      }else{
+        callLambdaResponse(linebot.generateConfirmMessage().then(function(confirmObj){
+          return lineClient.replyMessage(lineMessage.replyToken, confirmObj);
+        }), context);
+      }
+    }else if(lineMessage.type == "message"){
+      var replyMessageObjectPromise = linebot.checkConfirmed(lineMessage.source.userId).then(function(userData){
+        return linebot.searchVideoAndGenerateReplyMessageObject(lineMessage);
+      }).catch(function(userData) {
+        callLambdaResponse(linebot.generateConfirmMessage().then(function(confirmObj){
+          return lineClient.replyMessage(lineMessage.replyToken, confirmObj);
+        }), context);
+      });
+      if(!replyMessageObjectPromise) return;
+      callLambdaResponse(replyMessageObjectPromise.then(function(messageObj){
+        return lineClient.replyMessage(lineMessage.replyToken, messageObj);
+      }), context);
+    }
+  });
+};
